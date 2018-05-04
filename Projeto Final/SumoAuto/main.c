2bab6bd6360861729f64ee8b08f008d68b62b1e9
Gustavo Machado <gustavo.rodrigues.machado@usp.br>
2018-05-04 18:14:17 -0300

    Atualizações

--- Projeto Final/SumoAuto/main.c
+++ Projeto Final/SumoAuto/main.c
@@ -6,30 +6,31 @@
 #include "motors.h"
 #include "timer.h"
 
-#define VEL_MAX 500
-#define THRESHOLD 500
+#define VEL_MAX 150
+#define LINE_THRESHOLD 200
+#define DISTANCE_THRESHOLD 300
 
 int facing(){ //funçao que verifica se o adversario esta frente a frente
 
     update_distance_sensors();
 
-    return (distance_sensors[0] > THRESHOLD && distance_sensors[1] > THRESHOLD);
+    return (distance_sensors[0] > DISTANCE_THRESHOLD && distance_sensors[1] > DISTANCE_THRESHOLD);
 }
 
 void search(){ //funçao que gira o sumo ate ficar de frente para o adversario
 
     while (!facing()){
-        motors (0, VEL_MAX);
+        motors (0, VEL_MAX/2);
     }
     motors (0, 0);
 }
 
 void turn_left(){ //funçao para girar para a esquerda
-     motors(50, VEL_MAX);
+     motors(20, VEL_MAX);
 }
 
 void turn_right(){ //funçao para girar para a direita
-     motors(VEL_MAX, 50);
+     motors(VEL_MAX, 20);
 }
 
 int main () {
@@ -41,10 +42,13 @@ int main () {
 
         update_line_sensors();
 
-        if (line_sensors[0] > THRESHOLD) //se ler a linha do lado esquerdo, virar para a direita
+	if (line_sensors[0] < LINE_THRESHOLD && line_sensors[1] < LINE_THRESHOLD) //se ler a linha pelos dois sensores, anda para trás
+	    motors (-VEL_MAX, -VEL_MAX);
+
+	    else if (line_sensors[0] < LINE_THRESHOLD) //se ler a linha do lado esquerdo, virar para a direita
             turn_right();
 
-        if (line_sensors[1] > THRESHOLD) //se ler a linha do lado direito, virar para a esquerda
+	    else if (line_sensors[1] < LINE_THRESHOLD) //se ler a linha do lado direito, virar para a esquerda
             turn_left();
 
         if (facing()) //se estiver de frente para o adversario, acelera em direção a ele
